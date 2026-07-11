ad1ba8b5660b3a32a76066309bca6c7190447359
Dyyar-alt <Dyyar@yandex.ru>
2026-07-11 10:36:32 +0300

    Delete Models directory

--- Models/ChatMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Giga.Models
-{
-    public class ChatMessage
-    {
-        public string Sender { get; set; } = string.Empty;
-        public string Text { get; set; } = string.Empty;
-        public DateTime Timestamp { get; set; } = DateTime.Now;
-    }
-}
