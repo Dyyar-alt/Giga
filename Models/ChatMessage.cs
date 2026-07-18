c9ec2183796b8abf12ccbf690898c991aa2ec321
Dyyar-alt <Dyyar@yandex.ru>
2026-07-18 19:01:36 +0300

    Update ChatMessage.cs

--- Models/ChatMessage.cs
+++ Models/ChatMessage.cs
@@ -6,7 +6,7 @@
         public string Text { get; set; } = string.Empty;
         public DateTime Timestamp { get; set; } = DateTime.Now;
 
-        // Статистика токенов для сообщения
+        // Статистика токенов. Каждое сообщение потребляет токены. На сессию выдается 5000 токенов
         public int Tokens { get; set; }
         public int Characters { get; set; }
     }
